a6526a5c3204389a9fb7c653822b5645e93588ca
adityamaulana <aditmau26@gmail.com>
2023-05-08 11:12:22 +0700

    sec comm

--- pointer1/pointer1.cpp
+++ pointer1/pointer1.cpp
@@ -6,10 +6,26 @@ class mahasiswa
 {
     public : 
     int nim;
-    void showNIM()
+    void showNim()
         { 
         cout << "Nomor Induk = " << nim << endl; 
         }
         
 };
 
+int main() 
+
+{
+    mahasiswa mhs{1};
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs -> nim = 3;
+    mhs.showNim();
+
+    return 0;
+}
