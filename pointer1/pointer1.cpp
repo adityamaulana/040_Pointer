b2f8967cea6b35dec869614b4b78e972c8b68b2b
adityamaulana <aditmau26@gmail.com>
2023-05-08 10:58:51 +0700

    first comm

---
+++ pointer1/pointer1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+
+{
+    public : 
+    int nim;
+    void showNIM()
+        { 
+        cout << "Nomor Induk = " << nim << endl; 
+        }
+        
+};
+
